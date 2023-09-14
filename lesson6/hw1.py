1aa9690512f3c56bda485c5c3a8df2eb8f4a460f
stanislavfor <stasposter@narod.ru>
2023-09-14 05:57:16 +0300

    hw6 commit fix

--- lesson6/hw1.py
+++ lesson6/hw1.py
@@ -2,10 +2,16 @@
 # Ввод: 7 2 5
 # Вывод: 7 9 11 13 15
 
+# # Первый вариант решения
+# a1 = int(input("Первый элемент: "))
+# d = int(input("Шаг: "))
+# n = int(input("Количество элементов: "))
+# c = [a1 + (i - 1) * d for i in range(1, n + 1)]
+# print(*c)
 
-a1 = int(input("Первый элемент: "))
-d = int(input("Шаг: "))
-n = int(input("Количество элеменьтов: "))
-c = [a1 + (i - 1) * d for i in range(1, n + 1)]
-print(*c)
-
+# Второй вариант решения
+numbers = input("Первый элемент списка, шаг, количество элементов \n(введите через пробел): ")
+numbers = list(map(int, numbers.split()))
+res_array = [numbers[0] + (i - 1) * numbers[1] for i in range(1, numbers[2] + 1)]
+print(*res_array)
+# print()
